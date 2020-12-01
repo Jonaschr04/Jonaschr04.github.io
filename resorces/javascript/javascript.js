a82a6191091bfbfc4575285ed4c21b058cd41d61
Jonas Christensen <e-jonchr@iskule.no>
2020-12-01 08:58:47 +0100

    Initial commit

---
+++ resorces/javascript/javascript.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+var a = 5;
+var b = 10;
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(b%2);
+console.log(a==b);
+
+
+if(a <= 5) {
+    console.log("Tallet er mindre");
+} else if(a >= 5) {
+    console.log("tallet er høgyere");
+} 
