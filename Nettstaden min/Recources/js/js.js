872cda8718243c426109ddeefcec16d9aef6b958
Jonaschr04 <e-jonchr@iskule.no>
2021-04-23 13:03:35 +0200

    blblblblblbl

--- Nettstaden min/Recources/js/js.js
+++ Nettstaden min/Recources/js/js.js
@@ -1,4 +1,4 @@
-
+/*
 // oppgåve 1 a
 function syntaxError() {
     var tall = 5;
@@ -12,10 +12,11 @@ console.log(tall);
 
 
 // oppgåve 1 c
-var paragraph = document.getElementById();
+var paragraph = document.getElementById(); 
 
 
 // oppgåve 1 d
 var body = document.getElementByTagName("body")[0]; {
     body.Style.backgroundColor = "red";
 }
+*/
